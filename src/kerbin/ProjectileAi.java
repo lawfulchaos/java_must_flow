71fbefb85d09966c8d44ed06f991e4fd20f974a6
jack2018-2 <32910367+jack2018-2@users.noreply.github.com>
2019-04-27 13:36:51 +0300

    Projectil`ы перемещаются на три клетки, грамотно убиваются об стены но не атакуют

--- src/kerbin/ProjectileAi.java
+++ src/kerbin/ProjectileAi.java
@@ -12,17 +12,17 @@ public class ProjectileAi {
         for (int i =1; i<=3; i++){
             if (!world.tile(bullet.x+bullet.mx, bullet.y+bullet.my).isGround()) {
                 isHit = true;
-                bullet.mx = i * bullet.vectorx;
-                bullet.my = i * bullet.vectory;
                 break;
             }
+            bullet.mx = i * bullet.vectorx;
+            bullet.my = i * bullet.vectory;
         }
         bullet.x += bullet.mx;
         bullet.y += bullet.my;
         if(isHit) hit(world);
     }
     void hit(World world) {
-            if (world.creature(bullet.x, bullet.y) != null && world.creature(bullet.x, bullet.y).name == "player") {
+            if (world.creature(bullet.x, bullet.y) != null && world.creature(bullet.x, bullet.y).name == "Player") {
                 world.creature(bullet.x, bullet.y).hp -= bullet.dmg;
                 Event.getInstance().init("attacked from a distance", 5, 3, AsciiPanel.brightWhite);
                 bullet.getWorld().projectiles.remove(bullet);
