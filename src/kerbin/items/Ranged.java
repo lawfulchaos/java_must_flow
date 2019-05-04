13142d40ef2ce899dbb3dd7d7476875054412ccd
kerbalunion <kerbalspacema@gmail.com>
2019-05-04 13:55:26 +0300

    Fixes

--- src/kerbin/items/Ranged.java
+++ src/kerbin/items/Ranged.java
@@ -4,13 +4,12 @@ import kerbin.Creature;
 
 import java.awt.*;
 import java.io.Serializable;
-
 public class Ranged extends Weapon implements Serializable {
     public Ranged(char glyph, Color color, String name, Creature owner, int dmg, boolean isEquipable, String desc, int cost)
     {
         super(glyph, color, name, owner, dmg, isEquipable, desc, cost);
         if (modifier != null) {
-            this.dmg -= (Integer) modifier[2]* 1.5;
+            this.dmg += (Integer) modifier[2] / 3;
         }
     }
 }
