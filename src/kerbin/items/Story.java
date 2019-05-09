7761f677581f464a948be77a38565343d9634360
PolskeMbIwb <PolskeMbIwb@users.noreply.github.com>
2019-05-09 14:39:24 +0300

    Интегрирован сюжет за 2 копейки ААА класса

---
+++ src/kerbin/items/Story.java
@@ -0,0 +1,15 @@
+package kerbin.items;
+
+import kerbin.Creature;
+
+import java.awt.*;
+import java.io.Serializable;
+
+public class
+Story extends Item implements Serializable {
+
+    public Story(char glyph, Color color, String name, Creature owner, boolean isEquipable, String desc, int cost) {
+        super (glyph, color, name, owner, isEquipable, desc, cost);
+        modifier = null;
+    }
+}
